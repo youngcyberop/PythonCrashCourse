4fc373389a662a75a18fa325dc6ea930202a05dc
cyberop <5YbWdF9dgiqV>
2019-12-28 15:26:02 -0500

    ch2 name and variables

---
+++ ch2_name.py
@@ -0,0 +1,23 @@
+# Changing case in a string with methods
+
+name = "ada lovelace"
+print(name.title())
+
+#   name is the variable
+#   print is a function
+#   title is a method
+#   name.title passes the name variable to the title method to change case
+#   Additional parameters cna be passed to the method inside the brackets
+
+# Other useful methods
+
+# add output space
+print("")
+
+name = "Ada LoveLace"
+#   upper method capitalizes all characters in a string
+print(name.upper())
+#   lower() method changes all case to lower case in a string
+#   this can be important when storing data
+print(name.lower())
+
---
+++ ch2_variables.py
@@ -0,0 +1,13 @@
+# This is the first Python project created from Python Crash Course
+print("Hello Python interpreter!")
+
+# Variables
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+# Create error
+#message = "Hello Python Crash Course reader!"
+#print(mesage)
