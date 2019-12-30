9a087380756e46ab9a537cd22910a9db9c3ccd67
cyberop <5YbWdF9dgiqV>
2019-12-29 21:21:55 -0500

    Chapter 3

---
+++ Chapter 3 - Introducing Lists/tiy_3_2_greetings.py
@@ -0,0 +1,11 @@
+'''
+3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just
+printing each person’s name, print a message to them. The text of each message
+should be the same, but each message should be personalized with the
+person’s name.
+'''
+
+names = ['Erin', 'Dan', 'Sydney']
+print(f"Hello, {names[0]} how are you this evening?")
+print(f"What time is it, {names[1]}?")
+print(f"Are you hungry,{names[2]}?")
